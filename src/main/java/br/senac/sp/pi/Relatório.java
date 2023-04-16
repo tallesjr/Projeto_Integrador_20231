506f27a104213798ec9225547ec2c2bc2136870c
Denis <71805486+2D-nz@users.noreply.github.com>
2023-04-15 23:45:53 -0300

    Add files via upload

---
+++ src/main/java/br/senac/sp/pi/Relatório.java
@@ -0,0 +1,10 @@
+package br.senac.sp.pi;
+
+
+public class Relatório {
+
+    public static void main(String[] args) {
+        new telaRelatorio().setVisible(true);
+    }
+    
+}
---
+++ src/main/java/br/senac/sp/pi/TelaCadastroCliente.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package br.senac.sp.pi;
+import javax.swing.JOptionPane;
+/**
+ *
+ * @author denis.lsilva9
+ */
+public class TelaCadastroCliente {
+
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null, "oi");
+    }
+}
